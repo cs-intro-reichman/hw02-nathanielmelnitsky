f240f588ec3a9b4d2c42c085f5e13b605c3503b6
nathanielmelnitsky <nmelnitsky@gmail.com>
2024-03-21 09:27:20 +0200

    damka fix

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,13 +1,13 @@
 public class DamkaBoard {
 	public static void main(String[] args){
-		int given = Integer.parseInt(args[0]);
+		int given = 10; //Integer.parseInt(args[0]);
 
 		for(int i = 0; i < given; i++){
 				for(int j = 0; j < given; j++){
 					if(i % 2 == 0){
-						System.out.println("* ");
+						System.out.print("* ");
 					} else {
-						System.out.println(" *");
+						System.out.print(" *");
 					}
 
 					System.out.println();
