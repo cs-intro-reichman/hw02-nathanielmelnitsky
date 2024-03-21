13303e6d454df52a91f100a69caaf6a1ea3b8c76
nathanielmelnitsky <nmelnitsky@gmail.com>
2024-03-21 09:55:30 +0200

    change to stats second attempt

--- Perfect.java
+++ Perfect.java
@@ -1,36 +1,23 @@
+//sets up class
 public class Perfect {
-    public static void main(String[] args) {
-		//gets input
-        int given = Integer.parseInt(args[0]);
-		//will increment the candidate checking the remainder for divisibility
-        int candidateFactor = 1;
-		//used for check against remainder
-        int perfNumCheck = 0;
-
-		//loops finding divisors
-        while (candidateFactor < given) {
-            int remainder = given % candidateFactor;
-            if (remainder == 0) {
-                perfNumCheck += candidateFactor;
-            }
-			//increments to check next one
-            candidateFactor++;
-        }
-        //prints if perfect
-        if (perfNumCheck == given) {
-            System.out.println(given + " is a perfect number since " + given + " = 1");
-            candidateFactor = 2;
-			//loop used to format the perfect num output so it shows all divisors added
-            while (candidateFactor < given) {
-                if (given % candidateFactor == 0) {
-                    System.out.print(" + " + candidateFactor);
-                }
-                candidateFactor++;
-            }
-            System.out.println();
-			//simple output for if not perfect num
-        } else {
-            System.out.println(given + " is not a perfect number");
-        }
-    }
+	public static void main (String[] s) {
+        //gets num we want to check
+		int given = Integer.parseInt(s[0]);
+		int currentSum = 0;
+		String divisors = "";
+        //loops through numbers starting at first prime until the given
+		for (int i = 2; i <= given; i++) {
+            //if divisible by i and i is not given then we uppend
+			if (given % i == 0 && given != i) {
+				currentSum += i;
+				divisors += " + " + i;
+			}
+		}
+        //after all the uppending if the number is perfect prints with list of divisors
+		if (given == currentSum + 1) {
+			System.out.println(given + " is a perfect number since " + given + " = 1" + divisors);
+		} else {
+			System.out.println(given + " is not a perfect number");
+		}
+	}
 }
