77b32e6799aa79c7d549c663b410011a1b317b1c
nathanielmelnitsky <nmelnitsky@gmail.com>
2024-03-21 09:20:53 +0200

    march 21 change to damka board

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,17 +1,17 @@
 public class DamkaBoard {
 	public static void main(String[] args){
 		int given = Integer.parseInt(args[0]);
-		int i; //used for collumns
-		int j; //used for rows
 
-		for(i = 0; i < given; i++){
-				if(i % 2 != 0 && i != given - 1){
-					System.out.print(" ");
+		for(int i = 0; i < given; i++){
+				for(int j = 0; j < given; j++){
+					if(i % 2 == 0){
+						System.out.println("* ");
+					} else {
+						System.out.println(" *");
+					}
+
+					System.out.println();
 				}
-				for(j = 0; j < given; j++){
-				System.out.print("* ");
-				}
-			System.out.println();
 		}
 	}
 }
